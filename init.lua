59a9f3b648f61488a6e180f4fd76b89f541f11e6
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-06-11 08:32:27 +0800

    removed J

--- init.lua
+++ init.lua
@@ -87,6 +87,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 --MrG Keymaps
 vim.keymap.set('n', 'J', '<Nop>', { desc = 'Disable J key' })
+vim.api.nvim_set_keymap('n', 'J', '<Nop>', { noremap = true, silent = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -409,11 +410,7 @@ require('lazy').setup({
           --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
+          -- Jump to the type of the word under your cursor. Useful when you're not sure what type a variable is and you want to see the definition of its *type*, not where it was *defined*. map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
