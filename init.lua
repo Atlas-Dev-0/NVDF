e05dad1a2a7d87656c602338c876da4354e4c5e1
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-06-13 14:43:45 +0800

    i dunno why it does not work

--- init.lua
+++ init.lua
@@ -87,9 +87,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 --MrG Keymaps
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'remap select all' })
-vim.keymap.set('n', 'J', '<Nop>', { noremap = true, silent = true })
-vim.keymap.set('v', 'J', '<Nop>', { noremap = true, silent = true })
-vim.keymap.set('x', 'J', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('n', 'J', 'g5J', { noremap = true, silent = true })
+vim.keymap.set('v', 'J', 'g5J', { noremap = true, silent = true })
+vim.keymap.set('x', 'J', 'g5J', { noremap = true, silent = true })
+vim.keymap.set('n', 'K', 'g5k', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true })
 
 vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true })
