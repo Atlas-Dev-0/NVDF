3d3c3f6f9b232c8cc7bf704b72c581282cc8bf70
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-11-23 14:55:23 +0800

    removed selector

--- init.lua
+++ init.lua
@@ -214,24 +214,6 @@ require('lazy').setup({
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
-  --venv selector
-  --HACK: TO BE FIXED DUE TO SPAWN ERROR
-  {
-    'linux-cultist/venv-selector.nvim',
-    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-    opts = {
-      -- Your options go here
-      -- name = "venv",
-      -- auto_refresh = false
-    },
-    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    keys = {
-      -- Keymap to open VenvSelector to pick a venv.
-      { '<leader>vs', '<cmd>VenvSelect<cr>' },
-      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-    },
-  },
   -- Color Picker
   {
     'ziontee113/color-picker.nvim',
